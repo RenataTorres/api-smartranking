51423e8d0c119fbc087898ef57eefdb45377fb99
RenataTorres <renatatads2018@gmail.com>
2022-09-12 21:11:59 -0300

    feat: cria os endpoints

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common';
 import { CriarJogadorDto } from './interface/dtos/criar-jogador.dto';
+import { Jogador } from './interface/Jogador.interface';
 import { JogadoresService } from './jogadores.service';
 
 @Controller('api/v1/jogadores')
@@ -9,5 +10,20 @@ export class JogadoresController {
   @Post()
   async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
     await this.jogadoresService.criarAtualizarJogador(criarJogadorDto);
+  }
+
+  @Get()
+  async consultarJogadores(
+    @Query('email') email: string,
+  ): Promise<Jogador[] | Jogador> {
+    if (email) {
+      return this.jogadoresService.consultarJogadorPorEmail(email);
+    }
+    return this.jogadoresService.consultarTodosJogadores();
+  }
+
+  @Delete()
+  async deletarJogador(@Query('email') email: string): Promise<void> {
+    return this.jogadoresService.deletarJogador(email);
   }
 }
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './interface/dtos/criar-jogador.dto';
 import { Jogador } from './interface/Jogador.interface';
 import { v4 } from 'uuid';
@@ -10,7 +10,49 @@ export class JogadoresService {
   private jogadores: Jogador[] = [];
 
   async criarAtualizarJogador(criaJogadorDto: CriarJogadorDto): Promise<void> {
-    this.cria(criaJogadorDto);
+    const { email } = criaJogadorDto;
+
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+
+    if (jogadorEncontrado) {
+      this.atualizarJogador(jogadorEncontrado, criaJogadorDto);
+    } else {
+      this.cria(criaJogadorDto);
+    }
+  }
+
+  async consultarTodosJogadores(): Promise<Jogador[]> {
+    return this.jogadores;
+  }
+
+  async consultarJogadorPorEmail(email: string): Promise<Jogador> {
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+    if (!jogadorEncontrado) {
+      throw new NotFoundException(`Jogador com email ${email} não encontrado.`);
+    }
+    return jogadorEncontrado;
+  }
+
+  async deletarJogador(email: string): Promise<void> {
+    const jogadorEncontrado = this.jogadores.find(
+      (jogador) => jogador.email === email,
+    );
+    this.jogadores = this.jogadores.filter(
+      (jogador) => jogador.email !== email,
+    );
+  }
+
+  private atualizarJogador(
+    jogadorEncontrado: Jogador,
+    criaJogadorDto: CriarJogadorDto,
+  ): void {
+    const { nome } = criaJogadorDto;
+
+    jogadorEncontrado.nome = nome;
   }
 
   private cria(criaJogadorDto: CriarJogadorDto): void {
